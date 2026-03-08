e3af83a8d7c9a0bdcd1a795aee60ece6bec039c3
Tykhon Gusiev <tau.tihon@gmail.com>
2026-03-08 22:42:12 +0000

    Polling-based approach to grabbing absolute touch position

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 
 mod init;
-use evdev::EventType;
+use evdev::{EventType, AbsoluteAxisCode};
 
 use crate::init::TouchpadInfo;
 
@@ -19,11 +19,8 @@ fn main() {
     touchpad.y_min, touchpad.y_max);
     
     loop {
-        // let keys = touchpad.device.get_key_state()?;
-        // let finger_down = keys.contains(KeyCode::BTN_TOUCH);
-        
-        let events = match touchpad.device.fetch_events() {
-            Ok(events) => events,
+        let abs = match touchpad.device.get_abs_state() {
+            Ok(abs) => abs,
             Err(e) => match e.kind() {
                 std::io::ErrorKind::WouldBlock => continue,
                 std::io::ErrorKind::NotFound |
@@ -38,22 +35,29 @@ fn main() {
             }
         };
         
-        let mut x = 0;
-        let mut y = 0;
-        
-        for event in events {
-            if event.event_type() != EventType::ABSOLUTE {
-                continue;
+        let keystate = match touchpad.device.get_key_state() {
+            Ok(keystate) => keystate,
+            Err(e) => match e.kind() {
+                std::io::ErrorKind::WouldBlock => continue,
+                std::io::ErrorKind::NotFound |
+                std::io::ErrorKind::PermissionDenied => {
+                    eprintln!("Touchpad gone or inaccessible: {}", e);
+                    break;
+                }
+                _ => {
+                    eprintln!("Unexpected error: {}", e);
+                    break;
+                }
             }
-            
-            if( event.code() == 0 ) {x = event.value();}
-            if( event.code() == 1 ) {y = event.value();}
-            
-            // println!("code: {} | data: {}", event.code(), event.value());
+        };
+        
+        if( keystate.contains(evdev::KeyCode::BTN_TOUCH) ) {
+        
+        let x = abs[AbsoluteAxisCode::ABS_X.0 as usize].value;
+        let y = abs[AbsoluteAxisCode::ABS_Y.0 as usize].value;
+        println!("x: {} | y: {}", x, y);
         }
         
-        // if( finger_down ) {
-            println!("x: {} | y: {}", x, y);
-        // }
+        thread::sleep(Duration::from_millis(16));
     }
 }
